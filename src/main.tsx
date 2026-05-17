d1670dd26c11171dd9a522d15d68c606dd4b1e54
jasfine34 <jasfine34@gmail.com>
2026-05-17 17:44:34 -0400

    Force Lovable rebuild - bust build cache for stuck deploy 667b5c62

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+// BUILD_TAG: 2026-05-17T21:43:21Z - force rebuild for stuck Lovable deploy investigation
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
